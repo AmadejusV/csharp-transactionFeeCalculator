a520f34fc8b2b7ef66a0689add7d43a1e934d3ad
Amadejus Valionis <amadeus444@gmail.com>
2021-05-21 16:10:46 +0300

    refactor

--- transactionFeeCalculator/TransactionProcessor.cs
+++ transactionFeeCalculator/TransactionProcessor.cs
@@ -13,74 +13,98 @@ namespace transactionFeeCalculator
                 int transactionCount = 0;
                 while ((transaction = transactions.ReadLine()) != null)
                 {
-
-                    string[] words = transaction.Split(" ");
-                    string[] date = words[0].Split("-");
-                    int[] nums = new int[3];
-                    string merchant = words[1].ToUpper();
-                    double additionalDiscount = 0;
-                    double merchantDiscount = 0;
-                    bool weekend = false;
-                    //Console.WriteLine(merchant);
-
                     if (transactionCount < 10)
                     {
                         transactionCount++;
                     }
-                    else
-                    {
-                        additionalDiscount = 0.2;
-                    }
 
-                    if (merchant == "CIRCLE_K")
-                    {
-                        merchantDiscount = 0.2;
-                    }
-                    else if (merchant == "TELIA")
-                    {
-                        merchantDiscount = 0.1;
-                    }
-                    //Console.WriteLine($"{merchant} {discount}");
+                    string[] words = transaction.Split(" ");
+                    string[] givenDate = words[0].Split("-");
+                    int[] nums = new int[3];
+                    string merchant = words[1].ToUpper();
+                    //Console.WriteLine(merchant);
 
-                    for (int i = 0; i < date.Length; i++)
-                    {
-                        string dateNums = date[i];
-                        if (dateNums[0].Equals('0'))
-                        {
-                            dateNums = dateNums.Remove(0, 1);
-                            date[i] = dateNums;
-                        }
-
-                        int num = Convert.ToInt32(date[i]);
-                        nums[i] = num;
-                    }
-
-                    DateTime dateValue = new DateTime(nums[0], nums[1], nums[2]);
-                    int day = (int)dateValue.DayOfWeek;
-                    //Console.WriteLine(day);
-
-                    if (day == 6 || day == 0)
-                    {
-                        weekend = true;
-                    }
-                    //Console.WriteLine(weekend);
-
-                    double cost = Convert.ToDouble(words[2]);
-                    double fee = cost * 0.01;
-                    fee -= (fee * merchantDiscount);
-                    fee -= (fee * additionalDiscount);
-                    if (weekend)
-                    {
-                        fee = 0.00;
-                    }
-                    fee = Math.Round(fee, 2);
-                    //Console.WriteLine(fee);
+                    double additionalDiscount = CountAdditionalDiscount(transactionCount);
+                    double merchantDiscount = CountMerchantDiscount(merchant);
+                    nums = RemoveDateZeros(givenDate, nums);
+                    bool weekend = isWeekend(nums);
+                    double fee = CalculateTransactionFee(words, merchantDiscount, additionalDiscount, weekend);
 
                     writer.WriteLine($"{words[0]} {words[1]} {fee}");
                 }
             }
         }
-    }
 
+        public static double CalculateTransactionFee(string[] stringArray, double merchantDiscount, double additionalDiscount, bool isWeekend)
+        {
+            double cost = Convert.ToDouble(stringArray[2]);
+            double fee = cost * 0.01;
+            fee -= (fee * merchantDiscount);
+            fee -= (fee * additionalDiscount);
+            if (isWeekend)
+            {
+                fee = 0.00;
+            }
+            fee = Math.Round(fee, 2);
+            //Console.WriteLine(fee);
+            return fee;
+        }
+
+        public static double CountMerchantDiscount(string merchant)
+        {
+            double merchantDiscount=0;
+
+            if (merchant == "CIRCLE_K")
+            {
+                merchantDiscount = 0.2;
+            }
+            else if (merchant == "TELIA")
+            {
+                merchantDiscount = 0.1;
+            }
+            return merchantDiscount;
+        }
+
+        public static double CountAdditionalDiscount(int count)
+        {
+            double additionalDiscount = 0;
+            if (count < 10)
+            {
+                additionalDiscount = 0.2;
+            }
+            return additionalDiscount;
+        }
+
+        public static int[] RemoveDateZeros(string[] date, int[] numArray)
+        {
+            for (int i = 0; i < date.Length; i++)
+            {
+                string dateNumbers = date[i];
+                if (dateNumbers[0].Equals('0'))
+                {
+                    dateNumbers = dateNumbers.Remove(0, 1);
+                    date[i] = dateNumbers;
+                }
+
+                int num = Convert.ToInt32(date[i]);
+                numArray[i] = num;
+            }
+            return numArray;
+        }
+
+        public static bool isWeekend(int[] numArray)
+        {
+            bool itsWeekend = false;
+            DateTime dateValue = new DateTime(numArray[0], numArray[1], numArray[2]);
+            int day = (int)dateValue.DayOfWeek;
+            //Console.WriteLine(day);
+            if (day == 6 || day == 0)
+            {
+                itsWeekend = true;
+            }
+            //Console.WriteLine(weekend);
+            return itsWeekend;
+        }
+    }
 
 }
